92a4a95f741a4fb5745faadf7d60e08dce3a4d9b
Sam Catley <scp94373@gmail.com>
2021-02-13 15:51:26 +0000

    moved user file

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -6,7 +6,7 @@ set -e -u
 
 # Warning: customize_airootfs.sh is deprecated! Support for it will be removed in a future archiso version.
 
-sed -i 's/#\(en_UK\.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
 usermod -s /usr/bin/zsh root
